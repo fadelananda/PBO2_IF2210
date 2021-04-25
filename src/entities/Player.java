17cda9be4bc0268f99214cb25dc54fdae7c442cf
kadek surya mahardika <kadeksuryam@gmail.com>
2021-04-25 21:41:25 +0700

    testing breeding

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -193,16 +193,19 @@ public class Player {
     public Engimon breed(Engimon dad, Engimon mom){
         try{
             if (dad.getLevel() >= 4 && mom.getLevel() >= 4) {
-                String nama;
+                // list of inherited parent skills
                 Skill[] inheritedSkill = this.inheritSkill(dad, mom);
+                // resulting child element
                 EnumSet<Elements> inheritedElmt = this.inheritElement(dad, mom);
+                // resulting child species
                 String inheritedSpecies = this.inheritSpecies(inheritedElmt);
 
+                //Berikan nama child
+                String nama;
                 Scanner keyboard = new Scanner(System.in);
                 System.out.print("Masukkan nama buat engimon anak ini: ");
                 nama = keyboard.nextLine();
 
-                // Engimon child(nama, getX_pl(), getY_pl(), inheritedSpecies);
                 Engimon child = null;
                 if(inheritedSpecies == "Beckoo") child = new Beckoo(nama, this.plocation.getX(), this.plocation.getY());
                 else if(inheritedSpecies == "Geni") child = new Geni(nama, this.plocation.getX(), this.plocation.getY());
@@ -221,6 +224,7 @@ public class Player {
                 }
 
                 for(Skill s : inheritedSkill){
+                    if(s == null) continue;
                     child.addSkill(s);
                 }
 
@@ -252,12 +256,19 @@ public class Player {
             combinedSkill[i+4] = momSkill[i];
         }
 
-        Arrays.sort(combinedSkill, Comparator.comparingInt(Skill :: getMasteryLevel));
+        Arrays.sort(combinedSkill, Comparator.nullsFirst(Comparator.comparingInt(Skill::getMasteryLevel)));
         Collections.reverse(Arrays.asList(combinedSkill));
 
+        /*
+        for(Skill s : combinedSkill){
+            if(s == null) System.out.println("null");
+            else System.out.println(s.getName());
+        } */
         int nSkill = 0;
         for(Skill skill : combinedSkill) {
+            //jika skill sekarang termasuk skill unik engimon
             if(nSkill == 4) break;
+            if(skill == null) continue;
             if (dad.hasSkill(skill) && mom.hasSkill(skill)) {
                 retSkill[nSkill] = skill;
                 Skill currDadSkill = dad.getSkillByName(skill.getName());
