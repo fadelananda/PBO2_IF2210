9cab7c6c5018444e291b2e9dd097f3233d46beba
fadelananda <fadelananda123@gmail.com>
2021-04-25 15:31:24 +0700

    add player border

--- src/GUI/PlayerT.java
+++ src/GUI/PlayerT.java
@@ -4,6 +4,11 @@ public class PlayerT implements GameObject {
     /*FIELDS*/
     private Rectangle playerRect;
     int speed = 10;
+    static final int BORDER_UP = 10;
+    static final int BORDER_DOWN = 720;
+    static final int BORDER_LEFT = 10;
+    static final int BORDER_RIGHT = 950;
+
 
     /*METHODS*/
     public PlayerT(){
@@ -16,22 +21,22 @@ public class PlayerT implements GameObject {
     }
     public void update(Game game){
         KeyboardListener keyListener = game.getKeyListener();
-        if(keyListener.up()){
+        if(keyListener.up() && (playerRect.y >= BORDER_UP)){
             if(playerRect.y > 0){
                 playerRect.y -= speed;
             }
         }
-        if(keyListener.down()){
+        if(keyListener.down() && (playerRect.y <= BORDER_DOWN)){
             if(playerRect.y < 665){
                 playerRect.y += speed;
             }
         }
-        if(keyListener.left()){
+        if(keyListener.left() && (playerRect.x >= BORDER_LEFT)){
             if(playerRect.x > 0){
                 playerRect.x -= speed;
             }
         }
-        if(keyListener.right()){
+        if(keyListener.right() && (playerRect.x <= BORDER_RIGHT)){
             if(playerRect.x < 655){
                 playerRect.x += speed;
             }
