72e5179635643f27b8f72a3195f4eb0df7747bf8
Ryandito Diandaru <ryanditod@gmail.com>
2021-04-25 14:56:40 +0700

    set boundary aneh? ngatur biar ga keluar map masih nguli

--- src/MAP/PlayerT.java
+++ src/MAP/PlayerT.java
@@ -20,7 +20,7 @@ public class PlayerT implements GameObject {
             }
         }
         if(keyListener.down()){
-            if(playerRect.y < 672){
+            if(playerRect.y < 665){
                 playerRect.y += speed;
             }
         }
@@ -30,7 +30,7 @@ public class PlayerT implements GameObject {
             }
         }
         if(keyListener.right()){
-            if(playerRect.x < 672){
+            if(playerRect.x < 655){
                 playerRect.x += speed;
             }
         }
